467afc453288258af33c6b410f7f365a422a08e7
divyanshuu8 <singhdivyanshu975@gmail.com>
2024-11-29 14:39:54 +0530

    contact

--- src/components/FloatingCard.tsx
+++ src/components/FloatingCard.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
+import toast from "react-hot-toast"; // Import react-hot-toast
 
 interface FormData {
   name: string;
@@ -19,6 +21,15 @@ const FloatingFormCard: React.FC<MainProps> = ({ setShowForm }) => {
     mobile: "",
     service: "", // Initialize service field
   });
+  const [loading, setLoading] = useState(false); // Loading state to track form submission status
+  const [isMounted, setIsMounted] = useState(true); // Track whether the component is mounted
+
+  useEffect(() => {
+    // Cleanup function to set isMounted to false when the component unmounts
+    return () => {
+      setIsMounted(false);
+    };
+  }, []);
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
@@ -27,9 +38,75 @@ const FloatingFormCard: React.FC<MainProps> = ({ setShowForm }) => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Form Submitted:", formData);
+    setLoading(true); // Set loading state to true when submitting
+
+    // Form validation
+    if (!formData.name) {
+      toast.error("Name is required!");
+      setLoading(false); // Reset loading state
+      return;
+    }
+    if (!formData.email) {
+      toast.error("Email is required!");
+      setLoading(false); // Reset loading state
+      return;
+    }
+    if (!formData.mobile) {
+      toast.error("Mobile number is required!");
+      setLoading(false); // Reset loading state
+      return;
+    }
+    if (!formData.service) {
+      toast.error("Service selection is required!");
+      setLoading(false); // Reset loading state
+      return;
+    }
+
+    // Proceed with the form submission if validation passes
+    toast
+      .promise(
+        fetch("https://nodemailer-gmail.onrender.com/divyanshu", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        })
+          .then(async (response) => {
+            if (response.ok) {
+              const data = await response.json();
+              console.log("Form submitted successfully:", data);
+              setFormData({
+                name: "",
+                email: "",
+                mobile: "",
+                service: "",
+              });
+
+              if (isMounted) {
+                setShowForm(false); // Hide the form on successful submission
+              }
+
+              return "Form submitted successfully!"; // Return success message
+            } else {
+              throw new Error("Error submitting form");
+            }
+          })
+          .catch((error) => {
+            console.error("Form submission failed:", error);
+            throw new Error("Could not submit the form. Please try again.");
+          }),
+        {
+          loading: "Submitting form...",
+          success: <b>Form submitted successfully!</b>,
+          error: <b>Could not submit the form. Please try again.</b>,
+        }
+      )
+      .finally(() => {
+        setLoading(false); // Ensure loading is reset after the promise is settled (success or failure)
+      });
   };
 
   const handleClose = () => {
@@ -39,56 +116,74 @@ const FloatingFormCard: React.FC<MainProps> = ({ setShowForm }) => {
   return (
     <Overlay>
       <Card>
-        <CloseButton onClick={handleClose}>X</CloseButton>{" "}
+        <CloseButton onClick={handleClose}>X</CloseButton>
         {/* Cross button to close form */}
         <h2>Contact Us</h2>
         <Form onSubmit={handleSubmit}>
-          <Input
-            type="text"
-            name="name"
-            placeholder="Enter your name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type="email"
-            name="email"
-            placeholder="Enter your email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type="tel"
-            name="mobile"
-            placeholder="Enter your mobile number"
-            value={formData.mobile}
-            onChange={handleChange}
-            required
-            pattern="^\+?[1-9]\d{1,14}$" // Regex for valid phone number format
-            maxLength={15} // Limiting to 15 characters for international format
-          />
-          <Select
-            name="service"
-            value={formData.service}
-            onChange={handleChange}
-            required
-          >
-            <option value="">Select Service</option>
-            <option value="business Website Development">
-              Business Website Development
-            </option>
-            <option value="ecommerce Website Development">
-              Ecommerce Website Development
-            </option>
-          </Select>
-          <Button type="submit">Submit</Button>
+          <Label>
+            Name
+            <Input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </Label>
+
+          <Label>
+            Email
+            <Input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </Label>
+
+          <Label>
+            Mobile Number
+            <Input
+              type="tel"
+              name="mobile"
+              placeholder="+91 9879618851" // Added example number in the placeholder
+              value={formData.mobile}
+              onChange={handleChange} // Limiting to 15 characters for international format
+            />
+          </Label>
+
+          <Label>
+            Service
+            <Select
+              name="service"
+              value={formData.service}
+              onChange={handleChange}
+            >
+              <option value="">Select Service</option>
+              <option value="business Website Development">
+                Business Website Development
+              </option>
+              <option value="ecommerce Website Development">
+                Ecommerce Website Development
+              </option>
+            </Select>
+          </Label>
+
+          <Button type="submit">
+            {loading ? <RotatingCircle /> : "Submit"}
+          </Button>
         </Form>
       </Card>
     </Overlay>
   );
 };
+const Label = styled.label`
+  text-align: left;
+  font-size: 1rem;
+  margin-bottom: 5px;
+  color: #333;
+`;
 
 const Overlay = styled.div`
   position: fixed;
@@ -139,7 +234,7 @@ const CloseButton = styled.button`
 const Form = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 15px;
+  gap: 5px;
 `;
 
 const Input = styled.input`
@@ -147,6 +242,7 @@ const Input = styled.input`
   border: 1px solid #ccc;
   border-radius: 5px;
   font-size: 1rem;
+  width: 100%; /* Ensure inputs take up the full width of the container */
 `;
 
 const Select = styled.select`
@@ -154,6 +250,7 @@ const Select = styled.select`
   border: 1px solid #ccc;
   border-radius: 5px;
   font-size: 1rem;
+  width: 100%; /* Ensure select takes up full width */
 `;
 
 const Button = styled.button`
@@ -164,6 +261,27 @@ const Button = styled.button`
   font-size: var(--font-size-button);
   cursor: pointer;
   margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const RotatingCircle = styled.div`
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid #3498db;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  animation: spin 1s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
 
 export default FloatingFormCard;
