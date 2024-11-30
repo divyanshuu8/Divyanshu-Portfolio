0439801109417ecda3baaac7a7cd40b5b7758c29
divyanshuu8 <singhdivyanshu975@gmail.com>
2024-11-30 07:51:56 +0530

    proj

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import "react-toastify/dist/ReactToastify.css";
 import FloatingFormCard from "./components/FloatingCard";
 
 function App() {
-  const [showForm, setShowForm] = useState(false);
+  const [showForm, setShowForm] = useState(true);
 
   return (
     <>
